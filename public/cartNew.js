74eaf7cfe32ae979a29a64de0f7c5335809a2f34
Synetic1 <sinistersouls2922@gmail.com>
2023-04-17 18:03:00 +0100

    cart color

--- public/cartNew.js
+++ public/cartNew.js
@@ -1,9 +1,9 @@
-let label = document.getElementById('label');
-let ShoppingCart = document.getElementById("shopping-cart");
+let CartProduct = document.getElementById("cart-product");
 let Table = document.getElementById("table");
-let myCart = document.getElementById("my-cart");
-let CartContainer = document.getElementById("cart-page");
-
+let cartContainer = document.getElementById("cart-container");
+let TotalAmount = document.getElementById('TotalAmount');
+let subAmount = document.getElementById('subtotal');
+let FinalAmount = document.getElementById('FinalCost');
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
   
@@ -98,31 +98,70 @@ let calculation =()=>{
 calculation();
 
 let generateCartItems = ()=>{
-
     if(basket.length !==0){
-        myCart.innerHTML = `
-        
-            
-            <span class="v5_92">My Cart</span>
-            <div class="v5_91"></div>
-        `;
+
         Table.innerHTML = `
         
-        <div class="top" id="top">
-        <tr>
-            <th class="row-product" >Product</th>          <!-- product =50% of row, price,qnty,total,remove = other 50% --- 12.5% each  -->
-            <th class="other-row">Price</th>
-            <th class="other-row">Quantity</th>
-            <th class="other-row">Total</th>
-            <th class="other-row"><div></div></th>
-        </tr>
-        </div>
-        <div id="shopping-cart" class="shopping-cart">${(innerHTML= basket.map((x)=>{ 
+        ${(innerHTML= basket.map((x)=>{ 
             let {price, quantity} = x;
             let search = shopItemsData.find((y)=>y.price === price) || [];
             // console.log(search)
             return `
-        <tr> 
+            <div class="cart-product" id="cart-product">
+            <div class="cart-img">
+                <img src=${search.image} width="90%" height="70%">
+            </div>
+
+            <div class="cart-details">
+
+                <div class="name-quantity-price">
+                    <div class="name">${search.name} - ${search.size}</div>
+
+                    <div class="qp">
+
+                        <div class="select">
+                            <!--<img src="images/down-arrow (1).png" width="15px" height="15px" id="dropdownIcon">-->
+                            <select id="quantitySelect">
+                                <option value="0" onclick="removeItem(${price})">0</option>
+                                <option value="1" onclick="selectq(${price})">1</option>
+                                <option value="2" onclick="selectq(${price})">2</option>
+                                <option value="3" onclick="selectq(${price})">3</option>
+                            </select>
+                        </div>
+
+                        <!--<div class="select" id="select">
+                            <span id="quantityValue">0</span>
+                            <img src="images/down-arrow (1).png" width="15px" height="15px" alt="Dropdown Icon" id="dropdownIcon">
+                            <select id="quantitySelect">
+                                <option value="0" onclick="removeItem(${price})">0</option>
+                                <option value="1" onclick="selectq(${price})">1</option>
+                                <option value="2" onclick="selectq(${price})">2</option>
+                                <option value="3" onclick="selectq(${price})">3</option>
+                            </select>
+                        <input type="number" id="quantityInput" min="1" max="9999" step="1">
+                        </div>-->
+
+
+                        <div class="price-remove" id=${price}>
+                    
+                            <div class="price">£${search.cost}</div>
+                            <div class="remove" onclick="removeItem(${price})">Remove</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="delivery">
+                    <div class="delivery-icon">
+                        <img src="images/box.png" height="80%" width="100%">
+                    </div>
+                    <div class="col">
+                        <div>Order today</div>
+                        <div class="bold">Tue 18 Apr — Free</div>
+                    </div>
+                </div>
+            </div>
+
+
+       <!-- <tr>
             <td class="row-product">
                 
                 <div class="cart-info">
@@ -160,34 +199,29 @@ let generateCartItems = ()=>{
             </td>
 
 
-        </tr>
+        </tr> -->
             
-       
+        </div>
         `
     }).join(""))}
-    </div>
-    
 
 `;
 
 }
     else{
-        Table.innerHTML = `
+        cartContainer.innerHTML = `
 
-        <h2 class="title">Cart is empty</h2>
-        <a href="index.html" class="a">
-            <button class="btn"> back to shop </button>
-        </a>
+        <div class="bag-total">
+            <h2 class="total" >Your Cart is Empty</h2>
+            <a href="index.html" class="check-out">Continue Shopping</a>
+        </div>
 
         `;
-        myCart.innerHTML = ``;
-        CartContainer.innerHTML = `
-        `;
+        // cartContainer.style.display = 'none';
     }
 };
 
 generateCartItems();
-
 
 let totalAmount = () => {
 
@@ -200,89 +234,27 @@ let totalAmount = () => {
             return quantity * search.cost;
         })
         .reduce((x,y) => x + y,0);
-        console.log(amount)
-        label.innerHTML = `
+        console.log(amount + 'sdas')
 
-        <tr><div class="verysmallcontainer">
-            <div class="check"></div>
-            <div><p>SubTotal £${amount}</p></div>
-
-            <button id="checkout-btn" class="v5_117" >Checkout</button>
-        </div>
+        TotalAmount.innerText ='£' + amount;
+        subAmount.innerText ='£' + amount;
+        FinalAmount.innerText ='£' + amount;
         
-        </tr>
-
-
-
-
-        <!-- <div class="total-price">  onclick="getinfo()"  id="click"
-
-        <table>
-            <tr>
-                <td>Subtotal</td>
-                <td>£${amount}</td>
-            </tr>
-            <tr>
-                <td>Shipping</td>
-                <td>£200</td>
-            </tr>
-            <tr>
-                <td>Total</td>
-                <td>£${amount + 200}</td>
-            </tr>
-            <tr>
-            <form action="/create-checkout-session" method="POST">
-            <button type="submit" id="checkout-button" class="btn" onclick="checkoutintent()" >Checkout</button>
-            </form> 
-                <button class="btn" onclick="clearCart()" id="checkout" >Clear Cart</button>
-            </tr>
-            <tr><form action="/create-checkout-session" method="POST">
-            <button type="submit" id="checkout-button">Checkout</button>
-          </form></tr>
-
-          
-        </table>
-    </div> -->
-
-        `;
     }
+    // for (let i = 0; i < TotalAmounts.length; i++) {
+    //     TotalAmounts[i].innerText ='£' + amount;
+    //     console.log(i)
+    // }
     }
 totalAmount();
 
 
+
 const YOUR_DOMAIN = 'http://localhost:4242';
 
-// const getBtn = document.getElementById('get')
-
-// getBtn.addEventListener('click', getInfo)
-
-// async function getInfo(e) {
-
-    //console.log(JSON.stringify(basket))
-    // const response = await fetch(YOUR_DOMAIN, {
-    //     method: 'POST',
-    //     body: JSON.stringify(basket),
-    //     headers: {
-    //         'Accept': 'application/json',
-    //         'Content-Type': 'application/json'
-    //     },
-        
-    // });
-
-//     fetch("/create-checkout-session", {
-//             method: 'POST',
-            
-//             headers: {
-//                 'Accept': 'application/json',
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify(basket),
-//     });
-// }
-
-
-const button = document.getElementById("checkout-btn")
-button.addEventListener("click", () => {
+const button = document.querySelectorAll(".check-out")
+for (let i = 0; i < button.length; i++) {
+button[i].addEventListener("click", () => {
     console.log("dhgudsgudgasdas")
 
     fetch('/create-checkout-session', {
@@ -303,3 +275,4 @@ button.addEventListener("click", () => {
         console.error(e.error)
     })
 })
+}
